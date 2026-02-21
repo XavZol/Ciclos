8768b970e7d0db1bafa771c4a689d010034c3c02
XavZol <alansolis707@gmail.com>
2026-02-20 18:36:06 -0600

    Programa que solicita números hasta teclear 0

--- Ciclos.java
+++ Ciclos.java
@@ -1,21 +1,19 @@
 
+
+/* Pedir númmeros hasta que se teclee un 0, mostrar la suma de todos los númmeros introducidos.  */
+
 import javax.swing.JOptionPane;
 
-/* Leer un número e indicar si es positivo o negativo. El proceso se repetira hasta que se introduzca un cero */
 public class Ciclos {
 
     public static void main(String[] args) {
-        int numero;
+        int numero, suma = 0;
 
-        numero = Integer.parseInt(JOptionPane.showInputDialog("Ingrese un numero: "));
+        do {
+            numero = Integer.parseInt(JOptionPane.showInputDialog("Digite un número: "));
+            suma = suma + numero;
+        } while(numero != 0);
 
-        while(numero != 0) {
-            if(numero > 0) {
-                System.out.println("El número "+numero+" es positivo");
-            } else {
-                System.out.println("El número "+numero+" es negativo");
-            }
-        numero = Integer.parseInt(JOptionPane.showInputDialog("Ingrese un numero: "));
-        }
+        System.out.println("La suma es: "+suma);
     }
 }
