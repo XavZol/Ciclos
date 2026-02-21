1eb762600d693dbb374deb6ce3925d656ba35ebc
XavZol <alansolis707@gmail.com>
2026-02-20 19:15:16 -0600

    Programa que muestra todos los números del 1 a N.

--- Ciclos.java
+++ Ciclos.java
@@ -1,28 +1,19 @@
 
 
-/* Pedir números hasta que se introduzca uno negativo, y calcular la media.  */
+/* Pedir un número N, y mostrar todos  los números del 1 al N.  */
 import javax.swing.JOptionPane;
 
 public class Ciclos {
 
     public static void main(String[] args) {
-        int numero, elementos = 0, suma = 0;
-        float media;
+        int numero, i;
 
         numero = Integer.parseInt(JOptionPane.showInputDialog("Digite un número: "));
 
-        while (numero >= 0) {
-            suma = suma + numero; // Suma Iterativa
-            elementos++; // Aumentamos en 1 elementos 
-
-            numero = Integer.parseInt(JOptionPane.showInputDialog("Digite otro número: "));
-        }
-
-        if (elementos == 0) {
-            System.out.println("Error! La división entre 0 no existe");
-        } else {
-            media = (float) suma/elementos;
-            System.out.println("La media es: "+media);
+        i = 1;
+        while(i <= numero) {
+            System.out.println(i);
+            i++;
         }
     }
 }
