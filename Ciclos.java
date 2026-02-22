21434c07391dbe3d2338dc7843d3cac38bddad81
XavZol <alansolis707@gmail.com>
2026-02-21 23:08:55 -0600

    Programa para gestionar facturas.

--- Ciclos.java
+++ Ciclos.java
@@ -1,21 +1,35 @@
 
 
-/*Pide un número (que debe estar entre 0 y 10 ) y mostrar la tabla de multiplicar de dicho número */
+/* Programa para gestionar facturas. En cada factura figura: el código del artículo, la cantidad venndida 
+en litros y el precio por litro. Se pide de 5 facturas introducidas: Facturación total, cantidad de litros 
+vendidos del artículo 1 y cuantas facturas se emitieron de más de $600. */
 import javax.swing.JOptionPane;
 
 public class Ciclos {
 
     public static void main(String[] args) {
-            int numero;
+        int codigo, litros, litrosArticulo1=0, conteoMas600=0;
+        float precioLitro, importeFactura, facturacionTotal=0;
 
-            do {
-                numero = Integer.parseInt(JOptionPane.showInputDialog("Digite un número: "));
-            } while(numero<0 || numero>10); 
+        for(int i=1; i<=5; i++) {
+            codigo = Integer.parseInt(JOptionPane.showInputDialog("Artículo N°"+i+"\nDigite el código: "));
+            litros = Integer.parseInt(JOptionPane.showInputDialog("Artículo N°"+i+"\nDigite la cantidad en litros: "));
+            precioLitro = Float.parseFloat(JOptionPane.showInputDialog("Artículo N°"+i+"\nDigite el precio por litro:  "));
 
-            JOptionPane.showMessageDialog(null, "La tabla de multilplicar del número "+numero+" es: ");
+            importeFactura = (float) litros * precioLitro;
+            facturacionTotal += importeFactura;
 
-            for(int i=1; i<=12; i++) {
-                JOptionPane.showMessageDialog(null, numero+" * "+i+" = "+numero*i);
+            if(codigo == 1) {
+                litrosArticulo1 += litros;
+            }
+            if(importeFactura > 600) {
+                conteoMas600++;
             }
         }
+
+        System.out.println("Resumen de ventas");
+        System.out.println("Facturación total: "+facturacionTotal);
+        System.out.println("Cantidad en litros vendidos del artículo 1: "+litrosArticulo1);
+        System.out.println("Cantidad de facturas mayores a 600 dolares "+conteoMas600);
+    }
 }
