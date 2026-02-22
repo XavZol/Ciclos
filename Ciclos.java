2c340c1cb4871d54c8022137939abc1bb6b8e463
XavZol <alansolis707@gmail.com>
2026-02-21 20:13:24 -0600

    Programa que calcula el factorial de algún número.

--- Ciclos.java
+++ Ciclos.java
@@ -1,17 +1,21 @@
 
 
-/* Diseñar un programa que muestre el producto de los 10 primeros números impares. */
+/* Pedir un número y calcular su factorial 5!= 1*2*3*4*5, 7!= 1*2*3*4*5*6*7 */
 import javax.swing.JOptionPane;
 
 public class Ciclos {
 
     public static void main(String[] args) {
-        long producto = 1;
+        int numero;
+        long factorial=1;
 
-        for(int i=1; i<= 20; i+=2) {
-            producto = producto + i;
+        numero = Integer.parseInt(JOptionPane.showInputDialog("Digite un número: "));
+
+        // 5! = 1*2*3*4*5
+        for (int i=1; i<= numero; i++ ) {
+            factorial *= i; //multiplicación iterativa
         }
 
-        System.out.println("El producto es: "+producto);
+        System.out.println("El factorial de "+numero+" es: "+factorial);
     }
 }
