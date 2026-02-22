a8ae732263c89b5f69626b7fb482d62f4c8ba9bc
XavZol <alansolis707@gmail.com>
2026-02-21 20:48:29 -0600

    Programa solicitando estaturas y edades, imprimiendo la media y cuantos son mayores que.

--- Ciclos.java
+++ Ciclos.java
@@ -1,25 +1,36 @@
 
 
-/* Pedir 10 sueldos. Mostrar su suma y cuantos hay mayores de $1000 */
+/* Dadas las edades y alturas de 5 alumnas, mostrar la edad y la estatura media, la cantidad de alumnos
+mayores de 18 años, y la cantidad de alumnos que miden más de 1.75 */
 import javax.swing.JOptionPane;
 
 public class Ciclos {
 
     public static void main(String[] args) {
-        float sueldo, suma=0;
-        int contador= 0;
+        int edad, sumaEdad=0, conteoMayor18=0, contadorMayor175=0;
+        float altura, sumaAltura=0, mediaEdad, mediaAltura;
 
-        for(int i=1; i<=10; i++) {
-            sueldo = Float.parseFloat(JOptionPane.showInputDialog("Digite su sueldo: "));
-            
-            if(sueldo > 1000) { //Si el sueldo supera a mil 
-                contador++; //Aumentamos uno a contador
+        for(int i=1; i<=5; i++) {
+            edad = Integer.parseInt(JOptionPane.showInputDialog("Alumno "+i+"\nDigite su edad: "));
+            altura = Float.parseFloat(JOptionPane.showInputDialog("Alumno "+i+"\nDigite su etatura: "));
+
+            sumaEdad += edad;
+            sumaAltura += altura;
+
+            if(edad > 18) { // Si la edad es mayor a 18
+                conteoMayor18++;
             }
-
-            suma += sueldo;
+            if(altura > 1.75) { // Si la altura es mayor a 1.75
+                contadorMayor175++;
+            }
         }
 
-        JOptionPane.showMessageDialog(null,"La suma de todos los sueldos es: $"+suma);
-        JOptionPane.showMessageDialog(null,"Cantidad de sueldos mayores a 1000: "+contador);
+        mediaEdad = (float) sumaEdad/5;
+        mediaAltura = (float) sumaAltura/5;
+
+        JOptionPane.showMessageDialog(null, "La edad Promedio es: "+mediaEdad);
+        JOptionPane.showMessageDialog(null, "La estatura Promedio es: "+mediaAltura);
+        JOptionPane.showMessageDialog(null, "La Cantidad de Alumnos mayores a 18 años es: "+conteoMayor18);
+        JOptionPane.showMessageDialog(null, "La Cantidad de Alumnos que miden más de 1.75m es: "+contadorMayor175);
     }
 }
